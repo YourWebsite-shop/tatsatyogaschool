73b10d1a914b754ea2feaad9a3b2b2d645c5b290
Saurabhkumargit <saurabhkumar.t21@gmail.com>
2025-01-22 22:16:11 +0530

    contact

--- src/components/contactUs/ContactUs.tsx
+++ src/components/contactUs/ContactUs.tsx
@@ -3,8 +3,24 @@ import React, { ReactElement } from "react";
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-import { Menu, MenuButton, MenuList, MenuItem, Button } from "@chakra-ui/react";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+  Stack,
+  Grid,
+  FormControl,
+  HStack,
+  Icon,
+  Input,
+  Textarea,
+  Checkbox,
+} from "@chakra-ui/react";
 import { IoChevronDown } from "react-icons/io5";
+import { Flex, Box, Heading, Text } from "@chakra-ui/react";
+import { color } from "framer-motion";
 
 const ContactDetail = ({
   icon,
@@ -55,193 +71,184 @@ const ContactUs = () => {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
-            {/* Form Section */}
-            <div>
-              <form className="">
-                <div className="flex sm:flex-row justify-between py-6 ">
-                  <div>
-                    <label
-                      htmlFor="name"
-                      className="block text-sm text-neutral-600 font-semibold"
-                    >
-                      Full Name *
-                    </label>
-                    <input
-                      type="text"
-                      id="name"
-                      name="name"
-                      required
-                      className="mt-1 block w-full py-1 pr-4 border-b border-b-neutral-800 focus:outline-none "
-                    />
-                  </div>
-                  <div>
-                    <label
-                      htmlFor="Phone"
-                      className="block text-sm font-semibold text-neutral-600"
-                    >
-                      Phone *
-                    </label>
-                    <input
-                      type="text"
-                      id="Phone"
-                      name="Phone"
-                      required
-                      className="mt-1 block w-full py-1 pr-4 border-b border-b-neutral-800 focus:outline-none "
-                    />
-                  </div>
-                </div>
+          <Flex
+            flexDirection={{ base: "column-reverse", lg: "row" }}
+            justifyContent="space-between"
+          >
+            {/* Left Box */}
 
-                <div className="flex sm:flex-row justify-between py-6">
-                  <div>
-                    <label
-                      htmlFor="email"
-                      className="block text-sm font-semibold text-neutral-600"
+            <Box w={"120%"}>
+              <Heading
+                fontSize={"24px"}
+                fontWeight={500}
+                mb={"10px"}
+                color={"#222222"}
+              >
+                We would love to hear from you.
+              </Heading>
+              <Text fontSize={"16px"} mb={"40px"} color={"#666666"}>
+                If you’ve got great products your making or looking to work with
+                us then drop us a line.
+              </Text>
+              <Box>
+                <Flex>
+                  <Stack spacing={{ base: "16px", lg: "30px" }}>
+                    <Grid
+                      templateColumns={{
+                        base: "repeat(1, 1fr)",
+                        md: "repeat(2, 1fr)",
+                      }}
+                      gap={{ base: "16px", lg: "30px" }}
                     >
-                      Email Address *
-                    </label>
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      required
-                      className="mt-1 block w-full py-1 pr-4 border-b border-b-neutral-800 focus:outline-none"
-                    />
-                  </div>
-                  <div className="pr-8">
-                    <label
-                      htmlFor="program"
-                      className="block text-sm font-semibold text-neutral-600 pb-4"
-                    >
-                      Program Interest *
-                    </label>
-                    <Menu>
-                      <MenuButton
-                        as={Button}
-                        rightIcon={<IoChevronDown />}
-                        w="full"
-                        py={3}
-                        px={4}
-                        borderBottom="2px"
-                        borderColor="#98A2B3"
+                      <FormControl
+                        isRequired
+                        backgroundColor={"#f2f2f2"}
+                        pb={1}
+                        width="100%"
+                        rounded={"md"}
                       >
-                        Select a Program
-                      </MenuButton>
-                      <MenuList w="full">
-                        <MenuItem>200 Hour YTT</MenuItem>
-                        <MenuItem>300 Hour YTT</MenuItem>
-                        <MenuItem>Drop-in classes</MenuItem>
-                      </MenuList>
-                    </Menu>
-                  </div>
-                </div>
-
-                <div>
-                  <label
-                    htmlFor="message"
-                    className="block text-sm font-semibold text-neutral-600"
-                  >
-                    Message
-                  </label>
-                  <textarea
-                    id="message"
-                    name="message"
-                    rows={4}
-                    className="mt-1 block w-full py-1 pr-4 border-b border-b-neutral-800 focus:outline-none"
-                  ></textarea>
-                </div>
-
-                <button
-                  type="submit"
-                  className="w-full bg-[#E67825] text-white py-3 px-6 rounded-md hover:bg-[#E6782590] transition-colors duration-300 mt-8"
-                >
-                  Send Message
-                </button>
-              </form>
-            </div>
-
-            {/* Contact Information Section */}
-            <div className="space-y-8 ">
-              <div className="bg-white p-8 rounded-lg shadow-sm">
-                <h3 className="text-[16px] sm:text-[36px] lg:text-[48px] font-semibold text-neutral-800 mb-6">
-                  Contact us
-                </h3>
-                <ContactDetail
-                  icon={
-                    <svg
-                      className="w-6 h-6 text-[#E67825] mt-1 mr-4 mb-2"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
+                        <HStack>
+                          <Input
+                            type="text"
+                            name="name"
+                            placeholder="Name"
+                            _placeholder={{ color: "#000000" }}
+                            border="none"
+                            color={"black"}
+                            width="100%"
+                            focusBorderColor="black"
+                          ></Input>
+                        </HStack>
+                      </FormControl>
+                      <FormControl
+                        isRequired
+                        backgroundColor={"#f2f2f2"}
+                        pb={1}
+                        width="100%"
+                        rounded={"md"}
+                      >
+                        <HStack>
+                          <Input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            _placeholder={{ color: "#000000" }}
+                            border="none"
+                            color={"black"}
+                            width="100%"
+                            focusBorderColor="black"
+                          ></Input>
+                        </HStack>
+                      </FormControl>
+                    </Grid>
+                    <FormControl
+                      isRequired
+                      backgroundColor={"#f2f2f2"}
+                      pb={1}
+                      width="100%"
+                      rounded={"md"}
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                        id="el-emzm7ion"
-                      ></path>
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                        id="el-fsdis13v"
-                      ></path>
-                    </svg>
-                  }
-                  title=""
-                  description="Laxman Jhula Road, Rishikesh, Uttarakhand, India"
-                />
-                <ContactDetail
-                  icon={
-                    <svg
-                      className="w-6 h-6 text-[#E67825] mt-1 mr-4 mb-2"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
+                      <HStack>
+                        <Textarea
+                          name="message"
+                          placeholder="Message"
+                          _placeholder={{ color: "#000000" }}
+                          border="none"
+                          color={"black"}
+                          width="100%"
+                          focusBorderColor="black"
+                          rows={6}
+                        ></Textarea>
+                      </HStack>
+                    </FormControl>
+                    <Checkbox mt={"-10px"}>
+                      Save my name, email, and website in this browser for the
+                      next time I comment.
+                    </Checkbox>
+                    <Button
+                      width={"fit-content"}
+                      color={"#FFFFFF"}
+                      bg={"#E67825"}
+                      fontWeight={400}
+                      px={"20px"}
+                      py={"10px"}
+                      rounded={"50px"}
+                      _hover={{
+                        bg: "#D56820",
+                      }}
+                      mb={"60px"}
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                        id="el-y5u3uyua"
-                      ></path>
-                    </svg>
-                  }
-                  title=""
-                  description="info@tatsatyoga.com"
-                />
-                <ContactDetail
-                  icon={
-                    <svg
-                      className="w-6 h-6 text-[#E67825] mt-1 mr-4"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                        id="el-5ngpp0si"
-                      ></path>
-                    </svg>
-                  }
-                  title=""
-                  description="+91 123 456 7890"
-                />
+                      Submit Now
+                    </Button>
+                  </Stack>
+                </Flex>
+              </Box>
+            </Box>
+            {/* Right Box */}
+            <Box>
+              <Text
+                fontSize={"18px"}
+                fontWeight={500}
+                mb={"24px"}
+                color={"#222222"}
+              >
+                Address
+              </Text>
+              <Text
+                fontSize={"18px"}
+                fontWeight={400}
+                color={"#666666"}
+                mb={"28px"}
+              >
+                <span className="font-semibold  mb-[15px] ">
+                  Tatsat Yoga School
+                </span>{" "}
+                <br />
+                Bairaj Neelkanth Road, Laxman Jhula, Rishikesh, Uttarakhand
+                249302
+              </Text>
 
-                <div className="flex flex-row gap-6 text-[20px] pt-6 ">
-                  <FaFacebook className="text-[#E67825]" />
-                  <FaInstagram className="text-[#E67825]" />
-                  <FaXTwitter className="text-[#E67825]" />
-                </div>
-              </div>
-            </div>
-          </div>
+              <Text
+                fontSize={"18px"}
+                fontWeight={500}
+                mb={"24px"}
+                color={"#222222"}
+              >
+                Information
+              </Text>
+              <Text
+                fontSize={"18px"}
+                fontWeight={400}
+                mb={"28px"}
+                color={"#666666"}
+              >
+                <span className="mb-[14px]">+(91) 70888 55572</span> <br />
+                tatsatyogaschool.com
+              </Text>
+
+              <Text
+                fontSize={"18px"}
+                fontWeight={500}
+                mb={"24px"}
+                color={"#222222"}
+              >
+                Social Media
+              </Text>
+              <Flex
+                flex={"row"}
+                fontSize={"18px"}
+                fontWeight={400}
+                mb={"60px"}
+                gap={"30px"}
+                color={"#666666"}
+              >
+                <FaInstagram size={"24px"} />
+                <FaFacebook size={"24px"} />
+                <FaXTwitter size={"24px"} />
+              </Flex>
+            </Box>
+          </Flex>
+
           {/* map */}
           <div className="h-[60vh] pt-8  ">
             <iframe
